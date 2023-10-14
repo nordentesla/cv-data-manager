bb5dc48fddc7f1a2ba077fff4460fb84c2452520
Nico Galenzoga <82202192+nordentesla@users.noreply.github.com>
2023-10-14 16:22:35 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+CV Data Manager
+
+GitHub Repository:
+https://github.com/nordentesla/cv-data-manager
+
+CV Data Manager for flexible management of relevant 
+skills and experiences for target job applications.
+"""
+
+
+import sys
+
+
+def main():
+    WIP()
+
+def WIP():
+    sys.exit("work in progress".upper(), _ExitCode = 1)
+
+if __name__ == "__main__":
+    main()
